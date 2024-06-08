a9d7feb7a82f2b866b382ffee054aba68cf96352
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-06-08 17:11:46 +0700

    [learn-fun-cobol] 01. Basic learn cobol b01

---
+++ les01/testCreateB01.cbl
@@ -0,0 +1,7 @@
+000100 IDENTIFICATION DIVISION.                                          000100
+000200 PROGRAM-ID. test-create-file-b01.                                 000200
+       *> This is comment line
+       PROCEDURE DIVISION.
+       A000-FIRST-PARA.
+           DISPLAY "Hello World!".
+       STOP RUN.
