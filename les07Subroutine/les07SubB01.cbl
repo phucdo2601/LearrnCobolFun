5fbe2a19a0fefd41a8f4f0fec4a7bb8d74b2fe4f
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-06-09 17:34:39 +0700

    [learn-fun-cobol] 07. Basic learn sub in cobol b01

---
+++ les07Subroutine/les07SubB01.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. LES07-SUB-B01.
+
+       DATA DIVISION.
+           WORKING-STORAGE SECTION.
+           01 WS-STUDENT-NO PIC 9(4) VALUE 1000.
+           01 WS-STUDENT-NAME PIC A(30) VALUE 'Phucdn'.
+
+       PROCEDURE DIVISION.
+           CALL 'les07SubB01Sub' USING WS-STUDENT-NO, WS-STUDENT-NAME.
+           DISPLAY 'Student Id : ' WS-STUDENT-NO
+              DISPLAY 'Student Name : ' WS-STUDENT-NAME.
+       STOP RUN.
